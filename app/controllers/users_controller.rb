2da88870caaa96e75c6a17960fcf33658729d7b5
Titouan Deschanels <titouan.deschanels@epitech.eu>
2025-04-11 20:27:30 +0200

    refactor: correct rubocop offenses

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,24 @@
+# frozen_string_literal: true
+
 class UsersController < ApplicationController
   def spotify
     spotify_user = RSpotify::User.new(request.env["omniauth.auth"])
-
-    @user = User.find_or_create_by(spotify_id: spotify_user.id) do |user|
-      user.name = spotify_user.display_name
-      user.email = spotify_user.email
-      user.spotify_id = spotify_user.id
-    end
-
+    @user = find_or_create_user(spotify_user)
     session[:user_id] = @user.id
     redirect_to user_path(@user)
   end
 
   def show
     @user = User.find(params[:id])
+  end
+
+  private
+
+  def find_or_create_user(spotify_user)
+    User.find_or_create_by(spotify_id: spotify_user.id) do |user|
+      user.name = spotify_user.display_name
+      user.email = spotify_user.email
+      user.spotify_id = spotify_user.id
+    end
   end
 end
